84b73139968747d644ba0c81e53f8bcd41a1cbfc
RohanDhother <rohandhother@gmail.com>
2022-11-07 22:51:16 +0000

    Added comments to work done today

--- mysite/racing_calendar/urls.py
+++ mysite/racing_calendar/urls.py
@@ -4,6 +4,7 @@ from racing_calendar import views, api
 
 app_name = 'racing_calendar'
 urlpatterns = [
+#url paths to the view function
 path('', views.mainPage, name='mainPage'),
 path('admin/', views.admin, name='admin'),
 ]
